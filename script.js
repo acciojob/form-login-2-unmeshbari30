0e933d7dbc52dfe6ab0fc5a749218b896a79e253
Unmesh Bari <unmeshbari5@gmail.com>
2025-12-15 00:51:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let first = document.getElementById('first')
+let last = document.getElementById('last')
+let contact = document.getElementById('contact')
+let email = document.getElementById('email')    
+
+let btn = document.getElementById("btn")
+
+btn.addEventListener('click', ()=>{
+    alert(`First Name: ${first.value} Last Name: ${last.value} Phone Number: ${contact.value} Email ID: ${email.value}`)
+})
